26c93175f6d4eb3824c6e4fa637ffa8502fd63d4
jiabailie <spotlightyrg@gmail.com>
2013-11-19 22:00:43 +0800

    update practice

--- practice/3.5.php
+++ practice/3.5.php
@@ -3,10 +3,10 @@
 
 	echo "<h2>Practice 1</h2><br />";
 	$products1 = array(	
-			array( 'TIR', 'Tires', 100),
-			array( 'OIL', 'Oil', 10),
-			array( 'SPK', 'Spark Plugs', 4)
-		);
+		array( 'TIR', 'Tires', 100),
+		array( 'OIL', 'Oil', 10),
+		array( 'SPK', 'Spark Plugs', 4)
+	);
 
 	for($row = 0; $row < 3; $row++) {
 		for($column = 0; $column < 3; $column++) {
@@ -17,19 +17,22 @@
 
 	echo "<h2>Practice 2</h2><br />";
 	$products2 = array(
-			array(	'Code' => 'TIR',
-					'Description' => 'Tires',
-					'Price' => 100
-			),
-			array(	'Code' => 'OIL',
-					'Description' => 'Oil',
-					'Price' => 10
-			),
-			array(	'Code' => 'SPK',
-					'Description' => 'Spark Plugs',
-					'Price' => 4
-			)
-		);
+		array(
+			'Code' => 'TIR',
+			'Description' => 'Tires',
+			'Price' => 100
+		),
+		array(
+			'Code' => 'OIL',
+			'Description' => 'Oil',
+			'Price' => 10
+		),
+		array(
+			'Code' => 'SPK',
+			'Description' => 'Spark Plugs',
+			'Price' => 4
+		)
+	);
 	for($row = 0; $row < 3; $row++){
 		echo '|' .$products2[$row]['Code'] .'|' .$products2[$row]['Description'] .'|' .$products2[$row]['Price'] .'|' .'<br />';
 	}
@@ -48,22 +51,22 @@
 	echo "<h2>Practice 3</h2><br />";
 	
 	$categories = array(
-			array(	
-				array('CAR_TIR', 'Tires', 100),
-				array('CAR_OIL', 'Oil', 10),
-				array('CAR_SPK', 'Spark Plugs', 4)
-			),
-			array(	
-				array('VAN_TIR', 'Tires', 120),
-				array('VAN_OIL', 'Oil', 12),
-				array('VAN_SPK', 'Spark Plugs', 5)
-			),
-			array(	
-				array('TRK_TIR', 'Tires', 150),
-				array('TRK_OIL', 'Oil', 15),
-				array('TRK_SPK', 'Spark Plugs', 6)
-			)
-		);
+		array(	
+			array('CAR_TIR', 'Tires', 100),
+			array('CAR_OIL', 'Oil', 10),
+			array('CAR_SPK', 'Spark Plugs', 4)
+		),
+		array(	
+			array('VAN_TIR', 'Tires', 120),
+			array('VAN_OIL', 'Oil', 12),
+			array('VAN_SPK', 'Spark Plugs', 5)
+		),
+		array(	
+			array('TRK_TIR', 'Tires', 150),
+			array('TRK_OIL', 'Oil', 15),
+			array('TRK_SPK', 'Spark Plugs', 6)
+		)
+	);
 	
 	for($layer = 0; $layer < 3; $layer++){
 		echo 'Layer ' .$layer .'<br />';
